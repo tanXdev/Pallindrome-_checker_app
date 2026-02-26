de3d9fa2d3f6b5bc10a61c602e14cc0567350d35
tanXdev <tv1325@srmist.edu.in>
2026-02-26 16:08:57 +0530

    tanish vadel Added HardCodePalindrome

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+// useCase 2:
+public class PalindromeCheckerApp {
+     public static void main (String[] args){
+         System.out.println("Welcome to Palindrome Checker AppManagement System");
+         System.out.println("Modify this logic to usecase 2");
+     }
+}
