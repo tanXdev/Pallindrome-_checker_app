38a7fbe028a4f043928dd60be72f3f067cc5a7cb
ak0215-ux <ak0215@srmist.edu.in>
2026-03-13 15:38:50 +0530

    Tanish Vadel Added UC5-StackBasedPalindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,43 +1,36 @@
-import java.util.Scanner;
+import java.util.Stack;
 
-public class UseCase4PalindromeCheckerApp {
+public class PalindromeCheckerApp {
 
-    /**
-     * Application entry point for UC4.
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
+        String input = "noon";
 
-        // Convert string to character array
-        char[] chars = input.toCharArray();
 
-        int start = 0;
-        int end = chars.length - 1;
+        Stack<Character> stack = new Stack<>();
+
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
 
         boolean isPalindrome = true;
 
-        // Two-pointer comparison
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
 
-        // Display result
+
         if (isPalindrome) {
-            System.out.println("The string is a palindrome.");
+            System.out.println(input + " is a palindrome.");
         } else {
-            System.out.println("The string is not a palindrome.");
+            System.out.println(input + " is not a palindrome.");
         }
-
-        scanner.close();
     }
 }
